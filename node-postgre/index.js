59d1fbbaf53f5178a12d0b1a380660be6e3e05a1
najmulazka <najmulazka225@gmail.com>
2023-09-28 23:12:31 +0700

    Menambahkan fitur untuk mengupdate data nasabah berdasarkan id nasabah

--- node-postgre/index.js
+++ node-postgre/index.js
@@ -1,14 +1,23 @@
-const { postsNasabah, indexNasabah } = require('./helper/crud');
+const { postsNasabah, indexNasabah, showNasabah, updateNasabah } = require('./helper/crud');
 
 async function main() {
   try {
     // //Input data
-    // let post = await postsNasabah('Najmul Azka', 'Laki-laki', '088823927579', 'najmul@gmail.com', 'Purbalingga');
-    // let newPost = await postsNasabah('Dea Lili', 'Perempuan', '084723927579', 'dea@gmail.com', 'Banyumas');
-    // console.log(newPost);
+    // let newPost = await postsNasabah('Najmul Azka', 'Laki-laki', '088823927579', 'najmul@gmail.com', 'Purbalingga');
+    let newPost = await postsNasabah('Dea Lili', 'Perempuan', '084723927579', 'dea@gmail.com', 'Banyumas');
+    console.log(newPost);
 
+    // Melihat semua data nasabah
     let posts = await indexNasabah();
     console.log(posts);
+
+    // Melihat data nasabah berdasarkan id
+    let post = await showNasabah(1);
+    console.log(post);
+
+    // Mengupdate data nasabah
+    let postUpdateNasabah = await updateNasabah('1', 'Tukiman', 'Laki-laki', '088823927579', 'najmulazka@gmail.com', 'Purbalingga');
+    console.log(postUpdateNasabah);
   } catch (err) {
     console.log(err);
   }
